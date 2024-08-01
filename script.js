3120d868cfe052aff609e5248388185351d73678
Renzo Crisanto <117412799+xXRenzoElProXx@users.noreply.github.com>
2024-08-01 12:26:54 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+document.getElementById('quantity').addEventListener('input', function () {
+    const repeatGroup = document.getElementById('repeatGroup');
+    repeatGroup.style.display = this.value > 1 ? 'block' : 'none';
+});
+
+document.getElementById('numberForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const minNumber = parseInt(document.getElementById('minNumber').value);
+    const maxNumber = parseInt(document.getElementById('maxNumber').value);
+    const quantity = parseInt(document.getElementById('quantity').value);
+    const allowRepeats = document.getElementById('allowRepeats').checked;
+
+    if (minNumber >= maxNumber || quantity <= 0) {
+        swal({
+            title: "ERROR",
+            text: "Ingrese valores válidos",
+            icon: "error",
+        });
+        return;
+    }
+
+    let numbers = [];
+
+    if (allowRepeats) {
+        for (let i = 0; i < quantity; i++) {
+            numbers.push(generateRandomNumber(minNumber, maxNumber));
+        }
+    } else {
+        if (quantity > (maxNumber - minNumber + 1)) {
+            alert('No hay suficientes números únicos en el rango');
+            return;
+        }
+
+        while (numbers.length < quantity) {
+            const randomNumber = generateRandomNumber(minNumber, maxNumber);
+            if (!numbers.includes(randomNumber)) {
+                numbers.push(randomNumber);
+            }
+        }
+    }
+
+    displayResult(numbers);
+});
+
+function generateRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function displayResult(numbers) {
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = `<p>${numbers.join('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp')}</p>`;
+}
